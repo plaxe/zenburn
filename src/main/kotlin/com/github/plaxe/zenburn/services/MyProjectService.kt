a2656d460e5b9ddf192409b3ca6e4de5d8a6aa16
GitHub Action <action@github.com>
2022-09-29 18:25:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/plaxe/zenburn/services/MyProjectService.kt
+++ src/main/kotlin/com/github/plaxe/zenburn/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.plaxe.zenburn.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.plaxe.zenburn.MyBundle
 
 class MyProjectService(project: Project) {
 
